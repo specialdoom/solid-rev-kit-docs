b30fef62084d86fe4a903bd58e059aed5e862eb4
Bogdan Bledea <bledea.bogdan97@gmail.com>
2022-02-04 19:00:44 +0200

    Update form section

--- src/sections/FormSection.tsx
+++ src/sections/FormSection.tsx
@@ -1,17 +1,28 @@
-import { Input, Icons, TextArea } from '@specialdoom/solid-rev-kit';
+import { Input, Icons, TextArea, Counter } from '@specialdoom/solid-rev-kit';
 import { Container } from '../Container';
 
 export const FormSection = () => (
 	<Container type='fluid' flex gap='16px' flexDirection='row' flexWrap='wrap'>
-		<Input />
-		<Input value="Value" />
-		<Input placeholder="Placeholder" />
-		<Input value="Disabled" disabled />
-		<Input placeholder="With icon" icon={<Icons.Lens />} />
+		<Container type='full' flex gap='16px' flexDirection='row' flexWrap='wrap'>
+			<Input />
+			<Input value="Value" />
+			<Input placeholder="Placeholder" />
+			<Input value="Disabled" disabled />
+			<Input placeholder="With icon" icon={<Icons.Lens />} />
+		</Container>
 
-		<TextArea />
-		<TextArea value="Value" />
-		<TextArea placeholder="Placeholder" />
-		<TextArea placeholder="Disabled" disabled />
-	</Container>
+		<Container type='full' flex gap='16px' flexDirection='row' flexWrap='wrap'>
+			<TextArea />
+			<TextArea value="Value" />
+			<TextArea placeholder="Placeholder" />
+			<TextArea placeholder="Disabled" disabled />
+			<TextArea placeholder="Six rows textarea" rows={6} />
+		</Container>
+
+		<Container type='full' flex gap='16px' flexDirection='row' flexWrap='wrap'>
+			<Counter defaultValue={6} />
+			<Counter defaultValue={1} minValue={-2} maxValue={2} />
+			<Counter defaultValue={2} disabled />
+		</Container>
+	</Container >
 );
