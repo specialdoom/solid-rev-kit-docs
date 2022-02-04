77d38a05d74666b655f4924205afe34f0641ecc6
specialdoom <41910815+specialdoom@users.noreply.github.com>
2022-02-04 08:18:56 +0000

    Deploying to gh-pages from @ specialdoom/solid-rev-kit-docs@d77c589f4cb2654b75f828a5896aeb7065af9bbc 🚀

--- bundle.es.js
+++ bundle.es.js
@@ -1179,7 +1179,7 @@ function cleanChildren(parent, current, marker, replacement) {
   return [node];
 }
 
-const _tmpl$$2 = template(`<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C7.44772 0 7 0.447715 7 1V7H1C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9H7V15C7 15.5523 7.44772 16 8 16C8.55228 16 9 15.5523 9 15V9H15C15.5523 9 16 8.55228 16 8C16 7.44772 15.5523 7 15 7H9V1C9 0.447715 8.55228 0 8 0Z"></path></svg>`),
+const _tmpl$$1$1 = template(`<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M8 0C7.44772 0 7 0.447715 7 1V7H1C0.447715 7 0 7.44772 0 8C0 8.55228 0.447715 9 1 9H7V15C7 15.5523 7.44772 16 8 16C8.55228 16 9 15.5523 9 15V9H15C15.5523 9 16 8.55228 16 8C16 7.44772 15.5523 7 15 7H9V1C9 0.447715 8.55228 0 8 0Z"></path></svg>`),
       _tmpl$2$1 = template(`<svg width="16" height="12" viewBox="0 0 16 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0 1C0 0.447715 0.447715 0 1 0H15C15.5523 0 16 0.447715 16 1C16 1.55228 15.5523 2 15 2H1C0.447715 2 0 1.55228 0 1ZM0 6C0 5.44772 0.447715 5 1 5H15C15.5523 5 16 5.44772 16 6C16 6.55228 15.5523 7 15 7H1C0.447715 7 0 6.55228 0 6ZM1 10C0.447715 10 0 10.4477 0 11C0 11.5523 0.447715 12 1 12H15C15.5523 12 16 11.5523 16 11C16 10.4477 15.5523 10 15 10H1Z"></path></svg>`),
       _tmpl$3 = template(`<svg width="14" height="16" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M0.292893 0.292893C-0.0976311 0.683418 -0.0976311 1.31658 0.292893 1.70711L5.58579 7L0.292893 12.2929C-0.0976309 12.6834 -0.0976309 13.3166 0.292893 13.7071C0.683418 14.0976 1.31658 14.0976 1.70711 13.7071L7 8.41421L12.2929 13.7071C12.6834 14.0976 13.3166 14.0976 13.7071 13.7071C14.0976 13.3166 14.0976 12.6834 13.7071 12.2929L8.41421 7L13.7071 1.70711C14.0976 1.31658 14.0976 0.683418 13.7071 0.292893C13.3166 -0.0976311 12.6834 -0.0976311 12.2929 0.292893L7 5.58579L1.70711 0.292893C1.31658 -0.0976311 0.683418 -0.0976311 0.292893 0.292893Z"></path></svg>`),
       _tmpl$4 = template(`<svg width="4" height="16" viewBox="0 0 4 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M2 4C3.10457 4 4 3.10457 4 2C4 0.89543 3.10457 0 2 0C0.89543 0 0 0.89543 0 2C0 3.10457 0.89543 4 2 4ZM2 11C3.10457 11 4 10.1046 4 9C4 7.89543 3.10457 7 2 7C0.89543 7 0 7.89543 0 9C0 10.1046 0.89543 11 2 11ZM4 16C4 17.1046 3.10457 18 2 18C0.89543 18 0 17.1046 0 16C0 14.8954 0.89543 14 2 14C3.10457 14 4 14.8954 4 16Z"></path></svg>`),
@@ -1190,7 +1190,7 @@ const RevIcon = {
   Plus: ({
     fill
   }) => (() => {
-    const _el$ = _tmpl$$2.cloneNode(true),
+    const _el$ = _tmpl$$1$1.cloneNode(true),
           _el$2 = _el$.firstChild;
 
     setAttribute(_el$2, "fill", fill);
@@ -1825,7 +1825,7 @@ const InputContainer = styled('div')`
 	height: 52px;
 	outline: unset;
 	border-radius: 6px;
-	background: ${props => props.theme.colors.bright};
+	background: ${props => props.disabled ? props.theme.colors.shade : props.theme.colors.bright};
 	border: 1px solid ${props => props.theme.colors.shade};
 	font-size: 16px;
 	box-sizing: border-box;
@@ -1853,29 +1853,49 @@ const StyledInput = styled('input')`
 	}
 `;
 const Input = ({
-  value,
-  placeholder,
-  disabled,
   icon,
-  onChange,
-  onBlur,
-  onInput
+  disabled,
+  ...rest
 }) => createComponent(InputContainer, {
+  disabled: disabled,
+
   get children() {
-    return [createComponent(StyledInput, {
-      value: value,
-      placeholder: placeholder,
-      disabled: disabled,
-      onChange: onChange,
-      onBlur: onBlur,
-      onInput: onInput
-    }), createComponent(Show, {
+    return [createComponent(StyledInput, rest), createComponent(Show, {
       when: icon,
       children: icon
     })];
   }
 
 });
+
+const StyledTextArea = styled('textarea')`
+	outline: unset;
+	background: ${props => props.theme.colors.bright};
+	border: 1px solid ${props => props.theme.colors.shade};
+	font-size: 16px;
+	padding: 16px;
+	border-radius: 6px;
+
+	&:focus {
+		outline: unset;
+		border: 2px solid ${props => props.theme.colors.accent};
+	}
+
+	&::placeholder {
+		color: ${props => props.theme.colors.muted};
+	}
+
+	&:disabled, &:disabled::placeholder {
+		color: ${props => props.theme.colors.secondary};
+		background: ${props => props.theme.colors.shade};
+	}
+`;
+const TextArea = ({
+  rows = 4,
+  ...rest
+}) => createComponent(StyledTextArea, mergeProps({
+  rows: rows
+}, rest));
 
 const StyledSpace = styled('div')`
 	display: inline-flex;
@@ -1991,8 +2011,6 @@ const Modal = ({
   }
 
 });
-
-template(`<div><div></div></div>`);
 
 const GlobalStyle = createGlobalStyles`
 	@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Sans');
@@ -5538,7 +5556,7 @@ tippy.setDefaultProps({
   render: render
 });
 
-const _tmpl$$3 = template(`<div></div>`);
+const _tmpl$$2 = template(`<div></div>`);
 const Tooltip = ({
   type = 'accent',
   title,
@@ -5556,7 +5574,7 @@ const Tooltip = ({
     });
   });
   return (() => {
-    const _el$ = _tmpl$$3.cloneNode(true);
+    const _el$ = _tmpl$$2.cloneNode(true);
 
     const _ref$ = divRef;
     typeof _ref$ === "function" ? _ref$(_el$) : divRef = _el$;
@@ -6187,6 +6205,13 @@ const FormSection = () => createComponent(Container, {
         return createComponent(Icons.Lens, {});
       }
 
+    }), createComponent(TextArea, {}), createComponent(TextArea, {
+      value: "Value"
+    }), createComponent(TextArea, {
+      placeholder: "Placeholder"
+    }), createComponent(TextArea, {
+      placeholder: "Disabled",
+      disabled: true
     })];
   }
 
@@ -6298,8 +6323,6 @@ const App = () => {
     }), null);
 
     insert$1(_el$, createComponent(TooltipsSection, {}), null);
-
-    insert$1(_el$, createComponent(Input, {}), null);
 
     return _el$;
   })();
